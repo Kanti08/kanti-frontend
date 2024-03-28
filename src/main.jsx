f15a5be8ff9bef027ffc3587f9f9f4c314e1429c
Kanti08 <kantikori455@gmail.com>
2024-03-29 01:02:08 +0530

    kkjl

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-// import { CartProvider } from './components/context/cart.jsx'
-// import { AuthProvider } from './components/context/auth.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+// import { CartProvider } from "./components/context/cart.jsx";
+// import { AuthProvider } from "./components/context/auth.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-{/* <AuthProvider>
-   
-  
-    <CartProvider> */}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <AuthProvider>
+  //   <CartProvider>
       <BrowserRouter>
         <App />
-        </BrowserRouter>
-//     </CartProvider>
-   
-
-
- 
-// </AuthProvider>
- 
-  
-)
+      </BrowserRouter>
+  //   </CartProvider>
+  // </AuthProvider>
+);
